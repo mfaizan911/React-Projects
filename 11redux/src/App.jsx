a0cb6466cdce8293cbd639a576bd56e0609faef6
mfaizan911 <mfaizan.vork@gmail.com>
2024-08-14 22:05:56 +0500

    react redux

---
+++ 11redux/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodos';
+
+function App() {
+
+
+  return (
+    <>
+    <h1>React Redux Toolkit</h1>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
