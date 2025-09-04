bc2bb6c400b4388d62d109bc56b4c633ef1e733c
mfaizan911 <mfaizan.webcraft@gmail.com>
2025-09-05 04:11:29 +0500

    awwwards setup

---
+++ awwwards/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+  <main>
+    <h1 className='text-5xl font-bold text-violet-300'>Welcome to Awwwards</h1>
+  </main>
+  )
+}
+
+export default App
