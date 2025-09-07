8572fcbcf638e2843b6504dc7e6d8d892318acf6
mfaizan911 <mfaizan.webcraft@gmail.com>
2025-09-08 02:26:01 +0500

    heros section

--- awwwards/src/App.jsx
+++ awwwards/src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import Hero from "./components/Hero"
 
 const App = () => {
   return (
-  <main>
-    <h1 className='text-5xl font-bold text-violet-300'>Welcome to Awwwards</h1>
-  </main>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <Hero/>
+    </main>
   )
 }
 
